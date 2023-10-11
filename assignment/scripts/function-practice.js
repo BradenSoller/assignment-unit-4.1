fb2c15824d2b6d031523a425402799bd42758cd9
Braden Soller <braden.soller18@gmail.com>
2023-10-11 16:24:06 -0500

    added console log

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,6 +15,7 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(){
+  console.log("in the helloName");
   return "Hello, Your Name!"
 }
 
